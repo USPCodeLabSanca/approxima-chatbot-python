8161f6a98282338d13ca31382cbb1c075176e603
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2020-08-01 15:26:15 -0300

    update pip requirements

--- src/main.py
+++ src/main.py
@@ -7,24 +7,44 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-def start(bot, update):
-    response_message = "=^._.^="
-    bot.send_message(
-        chat_id=update.message.chat_id,
-        text=response_message
-    )
+logging.getLogger(__name__)
 
 
-# def http_cats(bot, update, args):
-#     bot.sendPhoto(
+# def start(bot, update):
+#     response_message = "=^._.^="
+#     bot.send_message(
 #         chat_id=update.message.chat_id,
-#         photo=BASE_API_URL + args[0]
+#         text=response_message
 #     )
 
 
+# # def http_cats(bot, update, args):
+# #     bot.sendPhoto(
+# #         chat_id=update.message.chat_id,
+# #         photo=BASE_API_URL + args[0]
+# #     )
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    update.message.reply_text('Hi!')
+
+
+def help_command(update, context):
+    print(update.effective_user.id)
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+
 def unknown(bot, update):
-    response_message = "Meow? =^._.^="
+    response_message = "Não entendi! Por favor, use um comando (eles começam com '/')."
     bot.send_message(
         chat_id=update.message.chat_id,
         text=response_message
@@ -32,26 +52,18 @@ def unknown(bot, update):
 
 
 def main():
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.INFO,
-                        format="%(asctime)s [%(levelname)s] %(message)s")
+    updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 
-    updater = Updater(token=TELEGRAM_TOKEN)
-
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(
-        CommandHandler('start', start)
-    )
-    # dispatcher.add_handler(
-    #     CommandHandler('http', http_cats, pass_args=True)
-    # )
-    dispatcher.add_handler(
-        MessageHandler(Filters.command, unknown)
-    )
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(CommandHandler("help", help_command))
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, echo))
 
     updater.start_polling()
     logging.info("=== Bot running! ===")
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
     logging.info("=== Bot shutting down! ===")
 

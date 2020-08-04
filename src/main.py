5343e7acf1b39327a394b9a5a979a2dbf1437060
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2020-08-04 17:51:53 -0300

    better comments

--- src/main.py
+++ src/main.py
@@ -15,9 +15,9 @@ categories = ['Filmes', 'Séries', 'Shows', 'Jogos', 'Livros e Literatura',
               'Hardware', 'Software', 'Esportes', 'Dança', 'Música', 'Pintura e Desenho',
               'Culinária', 'Mão na massa (consertos, costura, tricô, etc.)', 'Casa e Jardim',
               'Pets', 'Compras', 'Trabalho voluntário', 'Hobbies e Lazer', 'Política',
-              'Finanças', 'Viagens e Turismo', 'Intercâmbio', 'Automóveis e Veículos',
-              'Esotérico e Holístico', 'Espiritualidade', 'Times do coração',
-              'Causas (ambientais, feminismo, vegan, etc.)', 'Moda',
+              'Finanças', 'Viagens e Turismo', 'Intercâmbio', 'Rolês Universitários',
+              'Automóveis e Veículos', 'Esotérico e Holístico', 'Espiritualidade',
+              'Times do coração', 'Causas (ambientais, feminismo, vegan, etc.)', 'Moda',
               'Empreenderismo e Negócios', 'Imobiliário', 'Artesanato', 'Fotografia',
               'História', 'Mitologia', 'Pessoas e Sociedade', 'Anime e Mangá']
 
@@ -38,6 +38,7 @@ def normalizeCategories(categories, num_per_row=1):
     return new_categories
 
 
+# Categorias que já estão na forma certa para produzir o teclado do Bot depois
 norm_categories = normalizeCategories(categories, 1)
 
 # Enable logging
@@ -55,11 +56,10 @@ REGISTER_NAME, REGISTER_BIO, CHOOSE_ACTION = range(3)
 CHOOSING = 3
 
 
-# start => Inicia o bot. Se a pessoa não estiver cadastrada na base de dados
-# (dá pra ver pelo ID do Tele), pede para ela fornecer
-# um nome, uma pequena descrição pessoal e, por último para escolher seus interesses iniciais.
-
-def help(update, context):
+def help_command(update, context):
+    '''
+    Mostra os comandos disponiveis
+    '''
     text = "/prefs - Retorna uma lista com todas as categorias de interesse. A partir dela que você poderá adicionar ou remover interesses.\n"
     text += "/show - Mostra uma pessoa que tem interesses em comum.\n"
     text += "/random - Mostra uma pessoa aleatória.\n"
@@ -70,12 +70,17 @@ def help(update, context):
     return CHOOSE_ACTION
 
 
-def start(update, context):
+def start_command(update, context):
+    '''
+    start => Inicia o bot. Se a pessoa não estiver cadastrada na base de dados
+    (dá pra ver pelo ID do Tele), pede para ela fornecer:
+    um nome, uma pequena descrição pessoal e, por último para escolher seus interesses iniciais.
+    '''
     # Checa se o usuário já está no BD
     if update.effective_user.id in BD:
         update.message.reply_text(
             "Bora começar a usar o aplicativo!\nMe diz: o que você quer fazer agora? :)\n")
-        help(update, context)
+        help_command(update, context)
 
     # Caso contrario, o usuario devera se registrar
     update.message.reply_text(
@@ -104,11 +109,12 @@ def register_bio(update, context):
     BD[update.effective_user.id] = context.user_data
     return CHOOSE_ACTION
 
-    # prefs => Retorna lista de interesses (caixa de seleção). A pessoa pode marcar
-    # ou desmarcar o que ela quiser.
 
-
-def prefs(update, context):
+def prefs_command(update, context):
+    '''
+    prefs => Retorna lista de interesses (caixa de seleção). A pessoa pode marcar
+    ou desmarcar o que ela quiser.
+    '''
     cats = context.user_data['interests']
 
     keyboard = [
@@ -126,7 +132,7 @@ def prefs(update, context):
     return CHOOSING
 
 
-def mark_category(update, context):
+def change_category_state(update, context):
     update.callback_query.answer()  # await for answer
 
     cats = context.user_data['interests']
@@ -160,49 +166,55 @@ def submit_selection(update, context):
     update.callback_query.answer()  # await for answer
 
     # Guarda as informacoes no BD
-    # codigo aqui... (ta funfando por conta da lista ser passada por referencia)
+    # codigo aqui... (atualmente funfa por conta da lista ser passada por referencia nas atribuições)
 
     update.effective_message.reply_text('Seus interesses foram atualizados!')
     return ConversationHandler.END
 
 
-# show => Mostra uma pessoa que tem interesses em comum (vai com base no ranking).
-# Embaixo, um botão para enviar a solicitação de conexão deve existir.
-
-
-def show(update, context):
+def show_person_command(update, context):
+    '''
+    show => Mostra uma pessoa que tem interesses em comum (vai com base no ranking).
+    Embaixo, um botão para enviar a solicitação de conexão deve existir.
+    '''
     update.message.reply_text('Mostrei um amigo')
     return CHOOSE_ACTION
 
-# random => Mostra uma pessoa aleatória. Embaixo, um botão para enviar a solicitação
-# de conexão deve existir.
 
+def get_random_person_command(update, context):
+    '''
+    random => Mostra uma pessoa aleatória. Embaixo, um botão para enviar a solicitação
+    de conexão deve existir.
+    '''
 
-def random(update, context):
+    # DICA: importar "random" e usar random.choice(lista_aqui)
     update.message.reply_text('Random!!!')
     return CHOOSE_ACTION
 
-# pending => Mostra todas as solicitações de conexão que aquela pessoa possui e
-# para as quais ela ainda não deu uma resposta. Mostra, para cada solicitação,
-# a descrição da pessoa e dois botões: conectar ou descartar).
 
-
-def pending(update, context):
-    update.message.reply_text('Mostrei os que faltam repsonde')
+def pending_command(update, context):
+    '''
+    pending => Mostra todas as solicitações de conexão que aquela pessoa possui e
+    para as quais ela ainda não deu uma resposta. Mostra, para cada solicitação,
+    a descrição da pessoa e dois botões: conectar ou descartar).
+    '''
+    update.message.reply_text('Mostrei os que faltam responder')
     return CHOOSE_ACTION
 
-# friends => Mostra o contato (@ do Tele) de todas as pessoas com que o usuário
-# já se conectou.
 
-
-def friends(update, context):
+def friends_command(update, context):
+    '''
+    friends => Mostra o contato (@ do Tele) de todas as pessoas com que o usuário
+    já se conectou.
+    '''
     update.message.reply_text('Mostrei suas conexoes')
     return CHOOSE_ACTION
 
-# mensagem ou comando desconhecido
-
 
 # def unknown(update, context):
+#     '''
+#     Mensagem ou comando desconhecido
+#     '''
 #     response_message = "Não entendi! Por favor, use um comando (eles começam com '/')."
 #     update.message.reply_text(response_message)
 
@@ -211,10 +223,14 @@ def main():
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 
     prefs_handler = ConversationHandler(
-        entry_points=[CommandHandler('prefs', prefs)],
+        entry_points=[CommandHandler('prefs', prefs_command)],
 
         states={
-            CHOOSING: [CallbackQueryHandler(mark_category, pattern='^[\\d]+$'), CallbackQueryHandler(submit_selection, pattern='^finish$')],
+            CHOOSING: [
+                CallbackQueryHandler(
+                    change_category_state, pattern='^[\\d]+$'),
+                CallbackQueryHandler(submit_selection, pattern='^finish$')
+            ],
         },
 
         fallbacks=[MessageHandler(Filters.text, submit_selection)],
@@ -225,34 +241,33 @@ def main():
     )
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=[CommandHandler('start', start_command)],
 
         states={
-            REGISTER_NAME: [MessageHandler(Filters.text,
-                                           register_name)],
+            REGISTER_NAME: [
+                MessageHandler(Filters.text, register_name)
+            ],
 
-            REGISTER_BIO: [MessageHandler(Filters.text,
-                                          register_bio)],
+            REGISTER_BIO: [
+                MessageHandler(Filters.text, register_bio)
+            ],
 
             CHOOSE_ACTION: [
                 prefs_handler,
-                CommandHandler('show', show),
-                CommandHandler('pending', pending),
-                CommandHandler('friends', friends),
-                CommandHandler('help', help),
+                CommandHandler('show', show_person_command),
+                CommandHandler('pending', pending_command),
+                CommandHandler('friends', friends_command),
+                CommandHandler('help', help_command),
             ],
         },
 
-        fallbacks=[CommandHandler('start', start)]
+        fallbacks=[CommandHandler('start', start_command)]
     )
 
     updater.dispatcher.add_handler(conv_handler)
 
     updater.start_polling()
     logging.info("=== Bot running! ===")
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
     logging.info("=== Bot shutting down! ===")
 

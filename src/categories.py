96375726f68e035ac23ca7c4319fb2138503afd9
Vitor Santana Cordeiro <vitorsanusp@gmail.com>
2020-08-31 15:43:45 -0300

    removed "policial" from subcategories of "series"

--- src/categories.py
+++ src/categories.py
@@ -1,121 +1,379 @@
-
-categories = {
-    "Anime e Mangá": [0, {}],
-    "Artesanato": [1, {}],
-    "Automóveis e Veículos": [2, {}],
-    "Beleza e Fitness": [3, {}],
-    "Casa e Jardim": [4, {}],
-    "Causas (ambientais, feminismo, vegan...)": [5, {}],
-    "Ciclismo": [6, {}],
-    "Ciência e Ensino (tópicos acadêmicos)": [7, {}],
-    "Compras": [8, {}],
-    "Culinária": [9, {}],
-    "Dança": [10, {}],
-    "Empreendedorismo e Negócios": [11, {}],
-    "Esotérico e Holístico": [12, {}],
-    "Espiritualidade": [13, {}],
-    "Esportes": [14, {}],
-    "Fantasia (RPG, senhor dos anéis, etc.)": [15, {}],
-    "Ficção científica": [16, {}],
-    "Filmes": [17, {
-        "Ação": [0],
-        "Aventura": [1],
-        "Terror": [2],
-        "Comédia": [3],
-        "Suspense": [4],
-        "Faroeste": [5],
-        "Musical": [6],
-        "Drama": [7],
-        "Guerra": [8],
-        "Ficção Científica": [9],
-        "Fantasia": [10],
-        "Documentário": [11],
-        "Animação": [12],
-        "Romance": [13]
-    }
+{
+    "Anime e Mangá": [
+        0,
+        {}
     ],
-    "Finanças": [18, {}],
-    "Fotografia": [19, {}],
-    "Hardware": [20, {}],
-    "História": [21, {}],
-    "Idiomas": [22, {}],
-    "Imobiliário": [23, {}],
-    "Intercâmbio": [24, {}],
-    "Jogos de cartas": [25, {}],
-    "Jogos de tabuleiro": [26, {}],
-    "Jogos eletrônicos": [27, {}],
-    "Livros e Literatura": [28, {
-        "Científico": [0],
-        "Épico": [1],
-        "Romance": [2],
-        "Suspense": [3],
-        "Biográfico": [4],
-        "Histórico": [5]
-    }
+    "Artesanato": [
+        1,
+        {}
     ],
-    "Mitologia": [29, {}],
-    "Moda": [30, {}],
-    "Mão na massa (consertos, costura, tricô, etc.)": [31, {}],
-    "Música": [32, {
-        "Funk": [0],
-        "Axé": [1],
-        "Blues": [2],
-        "Country": [3],
-        "Eletrônica": [4],
-        "Forró": [5],
-        "Gospel": [6],
-        "Hip Hop": [7],
-        "Jazz": [8],
-        "MPB": [9],
-        "Música Clássica": [10],
-        "Pagode": [11],
-        "Rap": [12],
-        "Reggae": [13],
-        "Samba": [14],
-        "Sertanejo": [15],
-        "K-pop": [16],
-        "J-rock": [17],
-        "Pop": [18],
-        "Rock": [19],
-        "MPB": [20]
-    }
+    "Automóveis e Veículos": [
+        2,
+        {}
     ],
-    "Pessoas e Sociedade": [33, {}],
-    "Pets": [34, {}],
-    "Pintura e Desenho": [35, {}],
-    "Política": [36, {}],
-    "Quadrinhos": [37, {}],
-    "Rolês universitários": [38, {}],
-    "Saúde": [39, {}],
-    "Shows": [40, {}],
-    "Software": [41, {}],
-    "Séries": [42, {
-        "Ação": [0],
-        "Aventura": [1],
-        "Terror/Horror": [2],
-        "Comédia": [3],
-        "Suspense": [4],
-        "Biográfica": [5],
-        "Criminal": [6],
-        "Drama": [7],
-        "Desenho Animado": [8],
-        "Esportes Radicais": [9],
-        "Ficção Científica": [10],
-        "Fantasia": [11],
-        "Documentário": [12],
-        "Faroeste": [13],
-        "Ficção Histórica": [14],
-        "Humor Negro": [15],
-        "Médica": [16],
-        "Mistério": [17],
-        "Musical": [18],
-        "Policial": [19],
-        "Política": [20],
-        "Pós-apocalíptica": [21],
-        "Romance": [22],
-        "Sitcom (ex: Friends, HIMYM, TBBT...)": [23]
-    }],
-    "Teatro": [43, {}],
-    "Trabalho voluntário": [44, {}],
-    "Viagens e Turismo": [45, {}]
+    "Beleza e Fitness": [
+        3,
+        {}
+    ],
+    "Casa e Jardim": [
+        4,
+        {}
+    ],
+    "Causas (ambientais, feminismo, vegan...)": [
+        5,
+        {}
+    ],
+    "Ciclismo": [
+        6,
+        {}
+    ],
+    "Ciência e Ensino (tópicos acadêmicos)": [
+        7,
+        {}
+    ],
+    "Compras": [
+        8,
+        {}
+    ],
+    "Culinária": [
+        9,
+        {}
+    ],
+    "Dança": [
+        10,
+        {}
+    ],
+    "Empreendedorismo e Negócios": [
+        11,
+        {}
+    ],
+    "Esotérico e Holístico": [
+        12,
+        {}
+    ],
+    "Espiritualidade": [
+        13,
+        {}
+    ],
+    "Esportes": [
+        14,
+        {}
+    ],
+    "Fantasia (RPG, senhor dos anéis, etc.)": [
+        15,
+        {}
+    ],
+    "Ficção científica": [
+        16,
+        {}
+    ],
+    "Filmes": [
+        17,
+        {
+            "Animação": [
+                0
+            ],
+            "Aventura": [
+                1
+            ],
+            "Ação": [
+                2
+            ],
+            "Comédia": [
+                3
+            ],
+            "Documentário": [
+                4
+            ],
+            "Drama": [
+                5
+            ],
+            "Fantasia": [
+                6
+            ],
+            "Faroeste": [
+                7
+            ],
+            "Ficção Científica": [
+                8
+            ],
+            "Guerra": [
+                9
+            ],
+            "Musical": [
+                10
+            ],
+            "Romance": [
+                11
+            ],
+            "Suspense": [
+                12
+            ],
+            "Terror": [
+                13
+            ]
+        }
+    ],
+    "Finanças": [
+        18,
+        {}
+    ],
+    "Fotografia": [
+        19,
+        {}
+    ],
+    "Hardware": [
+        20,
+        {}
+    ],
+    "História": [
+        21,
+        {}
+    ],
+    "Idiomas": [
+        22,
+        {}
+    ],
+    "Imobiliário": [
+        23,
+        {}
+    ],
+    "Intercâmbio": [
+        24,
+        {}
+    ],
+    "Jogos de cartas": [
+        25,
+        {}
+    ],
+    "Jogos de tabuleiro": [
+        26,
+        {}
+    ],
+    "Jogos eletrônicos": [
+        27,
+        {}
+    ],
+    "Livros e Literatura": [
+        28,
+        {
+            "Biográfico": [
+                0
+            ],
+            "Científico": [
+                1
+            ],
+            "Histórico": [
+                2
+            ],
+            "Romance": [
+                3
+            ],
+            "Suspense": [
+                4
+            ],
+            "Épico": [
+                5
+            ]
+        }
+    ],
+    "Mitologia": [
+        29,
+        {}
+    ],
+    "Moda": [
+        30,
+        {}
+    ],
+    "Mão na massa (consertos, costura, tricô, etc.)": [
+        31,
+        {}
+    ],
+    "Música": [
+        32,
+        {
+            "Axé": [
+                0
+            ],
+            "Blues": [
+                1
+            ],
+            "Country": [
+                2
+            ],
+            "Eletrônica": [
+                3
+            ],
+            "Forró": [
+                4
+            ],
+            "Funk": [
+                5
+            ],
+            "Gospel": [
+                6
+            ],
+            "Hip Hop": [
+                7
+            ],
+            "J-rock": [
+                8
+            ],
+            "Jazz": [
+                9
+            ],
+            "K-pop": [
+                10
+            ],
+            "MPB": [
+                11
+            ],
+            "Música Clássica": [
+                12
+            ],
+            "Pagode": [
+                13
+            ],
+            "Pop": [
+                14
+            ],
+            "Rap": [
+                15
+            ],
+            "Reggae": [
+                16
+            ],
+            "Rock": [
+                17
+            ],
+            "Samba": [
+                18
+            ],
+            "Sertanejo": [
+                19
+            ]
+        }
+    ],
+    "Pessoas e Sociedade": [
+        33,
+        {}
+    ],
+    "Pets": [
+        34,
+        {}
+    ],
+    "Pintura e Desenho": [
+        35,
+        {}
+    ],
+    "Política": [
+        36,
+        {}
+    ],
+    "Quadrinhos": [
+        37,
+        {}
+    ],
+    "Rolês universitários": [
+        38,
+        {}
+    ],
+    "Saúde": [
+        39,
+        {}
+    ],
+    "Shows": [
+        40,
+        {}
+    ],
+    "Software": [
+        41,
+        {}
+    ],
+    "Séries": [
+        42,
+        {
+            "Aventura": [
+                0
+            ],
+            "Ação": [
+                1
+            ],
+            "Biográfica": [
+                2
+            ],
+            "Comédia": [
+                3
+            ],
+            "Desenho Animado": [
+                4
+            ],
+            "Documentário": [
+                5
+            ],
+            "Drama": [
+                6
+            ],
+            "Esportes Radicais": [
+                7
+            ],
+            "Fantasia": [
+                8
+            ],
+            "Faroeste": [
+                9
+            ],
+            "Ficção Científica": [
+                10
+            ],
+            "Ficção Histórica": [
+                11
+            ],
+            "Humor Negro": [
+                12
+            ],
+            "Mistério": [
+                13
+            ],
+            "Musical": [
+                14
+            ],
+            "Médica": [
+                15
+            ],
+            "Policial": [
+                16
+            ],
+            "Política": [
+                17
+            ],
+            "Pós-apocalíptica": [
+                18
+            ],
+            "Romance": [
+                19
+            ],
+            "Sitcom (ex: Friends, HIMYM, TBBT...)": [
+                20
+            ],
+            "Suspense": [
+                21
+            ],
+            "Terror/Horror": [
+                22
+            ]
+        }
+    ],
+    "Teatro": [
+        43,
+        {}
+    ],
+    "Trabalho voluntário": [
+        44,
+        {}
+    ],
+    "Viagens e Turismo": [
+        45,
+        {}
+    ]
 }
